90013f8bdf88ce16f7c1c87e09ec954f2e97d887
Kagan Kuscu <kagan-kuscu@hotmail.com.tr>
2020-07-06 19:07:01 +0300

    When you click number button it writes to display done.

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnWriteNumber(view: View) {
+        var btnChoose = view as Button
+        var btnValue = txtDisplay.text.toString()
+
+        when(btnChoose.id) {
+            btn0.id -> {
+                btnValue += "0"
+            }
+            btn1.id -> {
+                btnValue += "1"
+            }
+            btn2.id -> {
+                btnValue += "2"
+            }
+            btn3.id -> {
+                btnValue += "3"
+            }
+            btn4.id -> {
+                btnValue += "4"
+            }
+            btn5.id -> {
+                btnValue += "5"
+            }
+            btn6.id -> {
+                btnValue += "6"
+            }
+            btn7.id -> {
+                btnValue += "7"
+            }
+            btn8.id -> {
+                btnValue += "8"
+            }
+            btn9.id -> {
+                btnValue += "9"
+            }
+            btnMinus.id -> {
+                btnValue = "-$btnValue"
+            }
+            btnDot.id -> {
+                btnValue += "."
+            }
+        }
+        txtDisplay.setText(btnValue)
     }
 }
